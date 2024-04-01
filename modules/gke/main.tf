34216c542309d5ac481f34de6a82cdf72c8d58b9
Grzegorz Opara <grzegorz.opara@gmail.com>
2024-04-01 07:16:28 +0000

    added autoscaler, node-provisioning and separated node pools for app and db

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -24,17 +24,56 @@ resource "google_container_node_pool" "app-node-pool" {
   version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
   node_count = var.node_pool_size
 
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 2
+  }
+
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
 
-    preemptible  = true
+    preemptible  = false
     machine_type = var.node_size
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
+    }    
+    labels = {
+      app = "flask"
+    }
+  }
+}
+
+resource "google_container_node_pool" "app-node-pool" {
+  name       = "app-node-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count = var.node_pool_size
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 1
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    preemptible  = false
+    machine_type = var.node_size
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }    
+    labels = {
+      app = "db"
     }
   }
 }
